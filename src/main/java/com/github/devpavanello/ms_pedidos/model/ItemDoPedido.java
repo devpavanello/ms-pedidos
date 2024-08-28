35ea9a27ae2bc64df90fa6c34816d6680caabfd7
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-28 08:36:29 -0300

    Third commit - Aula 34

--- src/main/java/com/github/devpavanello/ms_pedidos/model/ItemDoPedido.java
+++ src/main/java/com/github/devpavanello/ms_pedidos/model/ItemDoPedido.java
@@ -1,10 +1,8 @@
 package br.com.fiap.ms_pedidos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,6 +22,16 @@ public class ItemDoPedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    @NotNull(message = "Campo requerido")
+    @Positive(message = "Quantidade deve ser um número positivo")
     private Integer quantidade;
+
+    @Column(nullable = false)
+    @NotNull(message = "Campo requerido")
     private String descricao;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
 }
--- src/main/java/com/github/devpavanello/ms_pedidos/model/Pedido.java
+++ src/main/java/com/github/devpavanello/ms_pedidos/model/Pedido.java
@@ -1,12 +1,6 @@
 package br.com.fiap.ms_pedidos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -28,7 +24,15 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private LocalDateTime dataHora;
+
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Status status;
+
+    //relacionamento
+    @OneToMany(cascade=CascadeType.PERSIST, mappedBy="pedido")
+    private List<ItemDoPedido> itens = new ArrayList<>();
+
 }
